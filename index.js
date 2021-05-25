fe1c132b8ac385d3dd0736f8de8e17dd72a8f391
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-05-26 00:34:18 +0200

    Add User Base class

--- index.js
+++ index.js
@@ -14,21 +14,42 @@ class IdGenerator {
   }
 }
 
+class User {
+  // Base class for Hairdresser and Customer
   constructor(name, surname, email, password) {
     this.name = name;
     this.surname = surname;
     this.email = email;
     this.password = password;
-    this.id = ''; //   generate ID or use from MongoDB IS?
+    this.id = idGenerator.generate();
     this.createdAt = new Date();
     this.address = '';
     this.tel = '';
-    this.availability = ''; // combobox such as [weekdays, weekends, after 7 pm, etc... ]
-    this.experience = ''; // combobox such as [less than 1 year, 1 year, 2 year, etc... ]
-    this.videos = [];
     this.photos = [];
-    this.serviceArea = ''; // perimeter in km around a location
-    this.certificates = []; // image files
+    this.privateMessages = [];
+  }
+
+  uploadPhoto (photo) {
+    this.photos.push(photo)
+  }
+
+  likePhoto (photo) {
+    photo.likedBy.push(this)
+  }
+
+  tagPhoto (photo, user) {
+    photo.taggedUsers.push(user)
+  }
+
+  likeVideo (video) {
+    video.likedBy.push(this)
+  }
+
+  sendPM (user, message) {
+    user.privateMessages.push(message)
+  }
+}
+
     this.employerReferences = [];
     this.customerReviews = [];
     this.ratings = 0;
