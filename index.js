c7e57f56a28fa1337a6ab72e525899c47445080a
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-05-26 00:38:33 +0200

    Add business logic test codes

--- index.js
+++ index.js
@@ -178,3 +178,37 @@ class PrivateMessage {
   }
 }
 
+
+const idGenerator = new IdGenerator()
+const hairdresser = new Hairdresser('Hair', 'Dresser', 'hair@dresser.com', 'password')
+const customer = new Customer('Customer', 'Surname', 'customer@customer.com', 'customerPassword')
+
+console.log('\n----ID check----')
+console.log('Hairdresser ID:', hairdresser.id)
+console.log('Customer ID:', customer.id)
+
+console.log('\n----Hairdresser uploads photo to his portfolio----')
+const photo = new Photo('image.jpg')
+hairdresser.uploadPhoto(photo)
+console.log('Hairdresser`s photos: ', hairdresser.photos)
+
+console.log('\n----Hairdresser tags user to uploaded photo----')
+hairdresser.tagPhoto(photo, customer)
+console.log('Hairdresser`s photos: ', hairdresser.photos)
+
+console.log('\n----Customer likes Hairdresser`s photo----')
+customer.likePhoto(photo)
+console.log('Hairdresser`s photos: ', hairdresser.photos)
+
+console.log('\n----Customer posts Hairdresser Request----')
+const hairdresserRequest = new CustomerRequest(
+  customer,
+  'Hairdresser Request',
+  'I am looking for a mobile hairdresser on this weekends. Can anyone help me?')
+customer.postRequest(hairdresserRequest)
+console.log('Customers`s requests: ', customer.customerRequests)
+
+console.log('\n----Hairdresser replies Customer`s Request----')
+const replyToRequest = new ReplyToRequest(hairdresser, 'I can help! Please PM me!...')
+hairdresser.replyToCustomerRequest(hairdresserRequest, replyToRequest)
+console.log('Customers`s requests: ', customer.customerRequests)
