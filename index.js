a45cdd204caee1fcbb82a569dea3cdf1954c40b7
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-05-26 00:33:12 +0200

    Create IdGenerator class

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-class Hairdresser {
+class IdGenerator {
+  // SINGLETON design pattern
+  constructor() {
+    IdGenerator.id = 1000
+    if (IdGenerator.instance) {
+      return IdGenerator.instance
+    }
+    IdGenerator.instance = this
+  }
+
+  generate () {
+    IdGenerator.id += 1
+    return IdGenerator.id
+  }
+}
+
   constructor(name, surname, email, password) {
     this.name = name;
     this.surname = surname;
