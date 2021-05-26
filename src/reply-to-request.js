10600abfedcfa5568c87a78e5492d404f9c2ca39
serhat <36408498+serhatci@users.noreply.github.com>
2021-05-26 19:58:42 +0200

    Create ReplyToRequest class

---
+++ src/reply-to-request.js
@@ -0,0 +1,10 @@
+const UserPost = require('./user-post')
+
+class ReplyToRequest extends UserPost {
+  constructor(user, postMessage, id) {
+    super(user, postMessage, id)
+    this.photos = [] // optional
+  }
+}
+
+module.exports = ReplyToRequest
