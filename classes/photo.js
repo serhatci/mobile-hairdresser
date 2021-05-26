f5b08860273909e2b035818c606af00679ef0041
serhat <36408498+serhatci@users.noreply.github.com>
2021-05-26 12:21:25 +0200

    Create photo class

---
+++ classes/photo.js
@@ -0,0 +1,12 @@
+const idGenerator = require('./id-generator')
+
+class Photo {
+    constructor(fileName, id) {
+        this.id = id
+        this.fileName = fileName
+        this.taggedUsers = []
+        this.likedBy = []
+    }
+}
+
+module.exports = Photo
