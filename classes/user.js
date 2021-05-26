a1662320177ec9ee5bd8f8a1c56ad8282078a22b
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-05-26 12:40:57 +0200

    Add portfolio dynamic property

--- classes/user.js
+++ classes/user.js
@@ -49,6 +49,17 @@ class Hairdresser extends User {
         this.customerReviews = [];
     }
 
+    get portfolio () {
+        return `
+        Name: ${this.name} ${this.surname}
+        Address: ${this.address}
+        Telephone: ${this.tel}
+        
+        ${this.name} ${this.surname} currently has/have ${this.photos.length} photo(s)
+        ${this.name} ${this.surname} currently has/have ${this.videos.length} video(s)
+        ${this.name} ${this.surname} currently has/have ${this.customerReviews.length} Customer Review(s)`
+    }
+
     uploadVideo (video) {
         this.video.push(video)
     }
@@ -63,6 +74,7 @@ class Hairdresser extends User {
 
     replyToCustomerRequest (request, reply) {
         request.replies.push(reply)
+
     }
 
     adviseToCustomerRequest (request, reply) {
@@ -83,6 +95,7 @@ class Customer extends User {
     postRequest (request) {
         this.customerRequests.push(request)
     }
+
 
     rateHairdresser (hairdresser, rating) {
         hairdresser.ratings.push(rating)
