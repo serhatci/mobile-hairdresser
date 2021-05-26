55c1553a644e180dda69870858a8809d15f0ca83
serhat <36408498+serhatci@users.noreply.github.com>
2021-05-26 12:22:28 +0200

    Create video class

---
+++ classes/video.js
@@ -0,0 +1,12 @@
+class Video {
+    constructor(fileName, title, description) {
+        this.id = id.generate()
+        this.fileName = fileName
+        this.title = title
+        this.description = description
+        this.taggedUsers = []
+        this.likedBy = []
+    }
+}
+
+module.exports = Video
