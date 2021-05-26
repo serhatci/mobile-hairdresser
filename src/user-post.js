b64d56ac0a50cc6bd88a1e101d055e0652ceb192
serhat <36408498+serhatci@users.noreply.github.com>
2021-05-26 19:56:03 +0200

    Extract child classes to seperate files

--- src/user-post.js
+++ src/user-post.js
@@ -7,20 +7,5 @@ class UserPost {
     this.date = new Date()
   }
 }
-class CustomerRequest extends UserPost {
-  constructor(user, type, postMessage, id) {
-    super(user, postMessage, id)
-    this.type = type
-    this.replies = []
-    this.photos = [] // optional
-  }
-}
 
-class ReplyToRequest extends UserPost {
-  constructor(user, postMessage, id) {
-    super(user, postMessage, id)
-    this.photos = [] // optional
-  }
-}
-
-module.exports = { CustomerRequest, ReplyToRequest }
+module.exports = UserPost
