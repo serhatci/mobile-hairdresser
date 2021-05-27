4b86f61c16e05f64cf2f2fd3755125acd300eddf
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-05-27 16:20:14 +0200

    Fix format

--- src/hairdresser.js
+++ src/hairdresser.js
@@ -1,18 +1,18 @@
-const User = require('./user');
+const User = require('./user')
 
 class Hairdresser extends User {
   constructor(name, surname, email, password, id) {
-    super(name, surname, email, password, id);
-    this.availability = 'available'; // [weekdays, weekends, after 7 pm, etc... ]
-    this.experience = 'less than 1 year'; // [less than 1 year, 1 year, 2 year, etc... ]
-    this.serviceArea = undefined; // perimeter in km around a location
-    this.ratings = [];
-    this.videos = [];
-    this.photos = [];
-    this.certificates = [];
-    this.employerReferences = [];
-    this.customerReviews = [];
-    this.repliedRequests = [];
+    super(name, surname, email, password, id)
+    this.availability = 'available' // [weekdays, weekends, after 7 pm, etc... ]
+    this.experience = 'less than 1 year' // [less than 1 year, 1 year, 2 year, etc... ]
+    this.serviceArea = undefined // perimeter in km around a location
+    this.ratings = []
+    this.videos = []
+    this.photos = []
+    this.certificates = []
+    this.employerReferences = []
+    this.customerReviews = []
+    this.repliedRequests = []
   }
 
   get portfolio() {
@@ -20,33 +20,33 @@ class Hairdresser extends User {
         Name: ${this.name} ${this.surname}
         Address: ${this.address}
         Telephone: ${this.tel}
-        
+
         ${this.name} ${this.surname} currently has/have ${this.photos.length} photo(s)
         ${this.name} ${this.surname} currently has/have ${this.videos.length} video(s)
-        ${this.name} ${this.surname} currently has/have ${this.customerReviews.length} Customer Review(s)`;
+        ${this.name} ${this.surname} currently has/have ${this.customerReviews.length} Customer Review(s)`
   }
 
   uploadVideo(video) {
-    this.video.push(video);
+    this.video.push(video)
   }
 
   tagVideo(video, user) {
-    video.taggedUsers.push(user);
+    video.taggedUsers.push(user)
   }
 
   uploadCertificate(certificate) {
-    this.certificates.push(certificate);
+    this.certificates.push(certificate)
   }
 
   replyToCustomerRequest(request, reply) {
-    request.replies.push(reply);
-    this.repliedRequests.push(request);
+    request.replies.push(reply)
+    this.repliedRequests.push(request)
   }
 
   adviseToCustomerRequest(request, reply) {
-    request.replies.push(reply);
-    this.repliedRequests.push(request);
+    request.replies.push(reply)
+    this.repliedRequests.push(request)
   }
 }
 
-module.exports = Hairdresser;
+module.exports = Hairdresser
