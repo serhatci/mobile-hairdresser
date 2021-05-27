ad38671739553b63f9124fffb7140ecb55e34c03
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-05-27 16:20:48 +0200

    Fix format

--- src/photo.js
+++ src/photo.js
@@ -1,12 +1,10 @@
-const idGenerator = require('./id-generator');
-
 class Photo {
   constructor(fileName, id) {
-    this.id = id;
-    this.fileName = fileName;
-    this.taggedUsers = [];
-    this.likedBy = [];
+    this.id = id
+    this.fileName = fileName
+    this.taggedUsers = []
+    this.likedBy = []
   }
 }
 
-module.exports = Photo;
+module.exports = Photo
