fc68b65122b1fd73feab79a858091564083773ce
serhat <36408498+serhatci@users.noreply.github.com>
2021-05-28 09:27:05 +0200

    Add hairdresser property

--- src/rating.js
+++ src/rating.js
@@ -1,6 +1,7 @@
 class Rating {
-  constructor(user, rate) {
+  constructor(user, hairdresser, rate) {
     this.user = user
+    this.user = hairdresser
     this.rate = rate
     this.date = new Date()
   }
