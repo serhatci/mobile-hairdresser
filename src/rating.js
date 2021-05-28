3421cbf553d4e5bf1cf3ebf3a690983052b7a3ed
serhat <36408498+serhatci@users.noreply.github.com>
2021-05-28 11:17:07 +0200

    Add id property

--- src/rating.js
+++ src/rating.js
@@ -1,5 +1,6 @@
 class Rating {
-  constructor(user, hairdresser, rate) {
+  constructor(user, hairdresser, rate, id) {
+    this.id = id
     this.user = user
     this.user = hairdresser
     this.rate = rate
