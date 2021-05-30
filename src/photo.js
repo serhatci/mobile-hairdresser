5ca6f7346231bcbbf455f991eba50218941c44a1
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-05-30 11:42:03 +0200

    Add createdAt property

--- src/photo.js
+++ src/photo.js
@@ -1,6 +1,7 @@
 class Photo {
   constructor(fileName, description = '') {
     this.id = undefined
+    this.createdAt = new Date()
     this.fileName = fileName
     this.description = description
     this.taggedUsers = []
