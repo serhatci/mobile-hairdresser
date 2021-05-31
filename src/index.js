b9c2848d2a67d8ca5c2fd094d2fea87eafeba8ec
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-05-31 22:17:48 +0200

    Add haidresser deletes photo logic

--- src/index.js
+++ src/index.js
@@ -14,6 +14,11 @@ hairdresser.uploadPhotoToPortfolio(photo)
 hairdresser.uploadVideoToPortfolio(video)
 // console.log('\nHairdresser added new photo and video:\n ', hairdresser.portfolioPhotos, hairdresser.portfolioVideos)
 
+// Hairdresser deletes photo and video from his portfolio
+// hairdresser.deletePhotoFromPortfolio(photo)
+// hairdresser.deleteVideoFromPortfolio(video)
+// console.log('\nHairdresser added new photo and video:\n ', hairdresser.portfolioPhotos, hairdresser.portfolioVideos)
+
 // Hairdresser tags a user to the uploaded photo & video
 hairdresser.tagPhoto(photo, customer)
 hairdresser.tagVideo(video, customer)
