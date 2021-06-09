2bbcfd5a3e8475476851e6ccb9a9f009b9aa54a2
serhat <36408498+serhatci@users.noreply.github.com>
2021-06-09 12:07:06 +0200

    Import database-connection.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
+require('./database-connection')
+
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 
