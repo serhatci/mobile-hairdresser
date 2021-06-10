ad84bd53173a6f439e83be8582704bb31a397bff
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-06-10 20:02:32 +0200

    Remove autopopulate

--- src/models/user.js
+++ src/models/user.js
@@ -31,7 +31,6 @@ const UserSchema = new mongoose.Schema(
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Request',
-        autopopulate: true,
       },
     ],
     messageBox: {
