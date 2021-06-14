a620e7ed8384c3a9d7aa3b9e1392b5632c20028b
serhat <36408498+serhatci@users.noreply.github.com>
2021-06-14 18:15:33 +0200

    Add connectionString variable

--- src/database-connection.js
+++ src/database-connection.js
@@ -7,11 +7,16 @@ mongoose.set('useFindAndModify', false)
 const username = process.env.MONGODB_USERNAME
 const password = process.env.MONGODB_PASSWORD
 const dbName = process.env.MONGODB_DATABASE
+let connectionString = process.env.MONGODB_CONNECTION_STRING
+
+if (!connectionString) {
+  connectionString = `mongodb+srv://${username}:${password}@cluster0.knvjh.gcp.mongodb.net/${dbName}?retryWrites=true&w=majority`
+}
 
 mongoose.set('debug', true)
 
 mongoose
-  .connect(`mongodb+srv://${username}:${password}@cluster0.vnprm.mongodb.net/${dbName}?retryWrites=true&w=majority`, {
+  .connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
