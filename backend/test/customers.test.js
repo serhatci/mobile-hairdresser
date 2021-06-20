7eaf7dad66a2802ba4254786971c4890f0efcead
serhat <36408498+serhatci@users.noreply.github.com>
2021-06-20 18:02:59 +0200

    Add customerList.length > 0  to related tests

--- backend/test/customers.test.js
+++ backend/test/customers.test.js
@@ -23,23 +23,34 @@ describe('Customers endpoints', () => {
 
     it('should give max 10 customers', async () => {
       const customerList = (await request(app).get('/api/customers')).body
-      const customersExist = customerList.length <= 10
+      const lessThanTen = customerList.length <= 10
+      const customersExist = customerList.length > 0
 
       expect(customersExist).toBe(true)
+      expect(lessThanTen).toBe(true)
     })
 
     it('should filter customers by city', async () => {
       const customerList = (await request(app).get('/api/customers?city=Heilbronn')).body
+      const customersExist = customerList.length > 0
+
+      expect(customersExist).toBe(true)
       customerList.forEach(user => expect(user.address.city).toEqual('Heilbronn'))
     })
 
     it('should filter customers by state', async () => {
       const customerList = (await request(app).get('/api/customers?state=BW')).body
+      const customersExist = customerList.length > 0
+
+      expect(customersExist).toBe(true)
       customerList.forEach(user => expect(user.address.state).toEqual('BW'))
     })
 
     it('should filter customers by postcode', async () => {
       const customerList = (await request(app).get('/api/customers?postcode=74076')).body
+      const customersExist = customerList.length > 0
+
+      expect(customersExist).toBe(true)
       customerList.forEach(user => expect(user.address.postcode).toEqual(74076))
     })
 
@@ -88,58 +99,3 @@ describe('Customers endpoints', () => {
     })
   })
 })
-
-// it('get request to /users should list users', async () => {
-//   const userList = (await request(app).get('/users')).body
-//   const usersExist = userList.length > 0
-
-//   expect(usersExist).toBe(true)
-// })
-
-// it('user should be able to like a photo', async () => {
-//   // create a photo
-//   const photo = (await request(app).post('/photos').send({ filename: 'coyotivtestingsession.png' })).body
-//   console.log('-------------photo--', photo)
-
-//   // create a user
-//   const userWithPhoto = (
-//     await request(app)
-//       .post('/users')
-//       .send({
-//         name: 'PhotoOwnerUser' + Date.now(),
-//         age: 27,
-//         bio: 'Someone sharing photos.',
-//       })
-//   ).body
-//   console.log('-------------userWithPhoto--', userWithPhoto)
-
-//   // add the photo to that user
-//   await request(app).post(`/users/${userWithPhoto._id}/adds`).send({ photoId: photo._id })
-
-//   // create another user
-//   const likerUser = {
-//     name: 'Liker User' + Date.now(),
-//     age: 36,
-//     bio: 'Someone liking photos.',
-//   }
-
-//   const createdLikerUser = (await request(app).post('/users').send(likerUser)).body
-//   console.log('-------------createdLikerUser--', createdLikerUser)
-
-//   // like the photo with that another user
-//   await request(app).post(`/users/${createdLikerUser._id}/likes`).send({ photoId: photo._id })
-
-//   const finalPhotoUser = (await request(app).get(`/users/${userWithPhoto._id}/json`)).body
-//   console.log('-------------finalPhotoUser--', finalPhotoUser)
-
-//   const finalLikerUser = (await request(app).get(`/users/${createdLikerUser._id}/json`)).body
-//   console.log('-------------finalLikerUser--', finalLikerUser)
-
-//   expect(finalPhotoUser.photos.length).toBe(1)
-//   expect(finalLikerUser.likes.length).toBe(1)
-
-//   console.log('finalPhotoUser.photos[0].likedBy[0]._id', finalPhotoUser.photos[0].likedBy[0]._id)
-
-//   expect(finalPhotoUser.photos[0].likedBy[0]._id).toBe(finalLikerUser._id)
-//   expect(finalLikerUser.likes[0]).toBe(finalPhotoUser.photos[0]._id)
-// })
