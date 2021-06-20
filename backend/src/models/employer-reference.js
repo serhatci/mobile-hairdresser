0fc1a1cd0646a6319b21239ef952af438d08c13c
serhat <36408498+serhatci@users.noreply.github.com>
2021-06-20 16:24:38 +0200

    Fix property validations

--- backend/src/models/employer-reference.js
+++ backend/src/models/employer-reference.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose')
 
+const { isAlphanumeric, isEmail } = require('validator')
+
 const EmployerReferenceSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    default: null,
+    trim: true,
+    validate: [isAlphanumeric, 'Name should contain letters & numbers only'],
   },
-  shop: String,
-  address: String,
-  email: String,
-  telephone: String,
+  shop: { Type: String, default: null, trim: true },
+  address: { Type: String, default: null, trim: true },
+  email: { Type: String, default: null, trim: true, validate: [isEmail, 'Enter a valid email address'] },
+  telephone: { Type: Number, default: null, trim: true },
 })
 
 module.exports = EmployerReferenceSchema
