3dc10a52b248e6aef42faaeb887730397d34fa7f
serhat <36408498+serhatci@users.noreply.github.com>
2021-06-20 23:25:42 +0200

    Remove default values & validate tel

--- backend/src/models/employer-reference.js
+++ backend/src/models/employer-reference.js
@@ -1,19 +1,22 @@
 const mongoose = require('mongoose')
 
-const { isAlphanumeric, isEmail } = require('validator')
+const { isAlphanumeric, isEmail, isInt } = require('validator')
 
 const EmployerReferenceSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    default: null,
     trim: true,
     validate: [isAlphanumeric, 'Name should contain letters & numbers only'],
   },
-  shop: { type: String, default: null, trim: true },
-  address: { type: String, default: null, trim: true },
+  shop: { type: String, trim: true },
+  address: { type: String, trim: true },
   email: { type: String, trim: true, validate: [isEmail, 'Enter a valid email address'] },
-  telephone: { type: Number, default: null, trim: true },
+  tel: {
+    type: String,
+    trim: true,
+    validate: [isInt, 'Telephone should contain numbers only'],
+  },
 })
 
 module.exports = mongoose.model('EmployerReference', EmployerReferenceSchema)
