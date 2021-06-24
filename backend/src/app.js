2e5f515f09bc86e1165a79c8e0cde7caa4f51024
serhat <36408498+serhatci@users.noreply.github.com>
2021-06-25 01:24:08 +0200

    Implement session

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,8 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
 
 require('./database-connection')
 
@@ -11,6 +13,7 @@ const indexRouter = require('./routes/index')
 const customersRouter = require('./routes/customers')
 const hairdressersRouter = require('./routes/hairdressers')
 const photosRouter = require('./routes/photos')
+const accountsRouter = require('./routes/accounts')
 
 const app = express()
 
@@ -31,12 +34,25 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+
+app.use(
+  session({
+    secret: ['ofCourseThisIsDifferentInProduction', 'PushedHereOnlyForLEarningPurposes'],
+    store: MongoStore.create({ mongoUrl: 'mongodb://mongo/mobilehairdresser', stringify: false }),
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      path: '/api',
+    },
+  })
+)
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/api', indexRouter)
 app.use('/api/customers', customersRouter)
 app.use('/api/hairdressers', hairdressersRouter)
 app.use('/api/photos', photosRouter)
+app.use('/api/accounts', accountsRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
