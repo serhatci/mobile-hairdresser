05ec74e802ead427c7b98e094f5ddd99f30b8cfb
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-06-24 14:56:29 +0200

    Add new styles to app.vue

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -9,4 +9,30 @@
 <style lang="scss">
 @import './assets/theme.scss';
 @import 'bootstrap/scss/bootstrap.scss';
+
+.container-fluid {
+  min-width: var(--my-screen-width);
+}
+
+header {
+  padding: 0.4rem 3rem;
+  background-color: var(--my-blue);
+}
+
+header a {
+  color: var(--my-font-white);
+}
+
+footer {
+  background-color: var(--my-darkblue);
+}
+
+footer p {
+  min-height: 20vh;
+}
+
+.logo {
+  height: 45px;
+  min-width: 200px;
+}
 </style>
