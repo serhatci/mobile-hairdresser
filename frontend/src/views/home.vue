be5094c86c44537b40c7b7f7616c524183ca5b5a
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-06-24 15:01:33 +0200

    Transfer Header & Footer components to app.vue

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -1,15 +1,10 @@
 <script>
 import axios from 'axios'
-import Header from "@/components/header.vue";
-import Footer from "@/components/footer.vue";
 import HairdresserCard from "@/components/hairdresser-card.vue";
 
 export default {
-  path: '/',
   name: "Home",
   components: {
-    Header,
-    Footer,
     HairdresserCard
   },
   data () {
@@ -29,65 +24,59 @@ export default {
 </script>
 
 <template lang="pug">
-body
-  .container-fluid.g-0
-    header.sticky-top.shadow.border-bottom.border-secondary
-      Header
-    main
-      section.first
-        .row.g-0
-          .col.g-0
-            .position-relative
-              video.bg-video(playsinline='playsinline', autoplay='autoplay', muted='muted', loop='loop')
-                source(src='/img/videos/barbershop.mp4', type='video/mp4')
-            .search-card-container
-              .card.m-3.border-primary
-                h5.card-header.text-center I am looking for
-                .card-body
-                  .form.text-center(action='submit')
-                    .radiobox-container
-                      .row.align-items-center.mx-2
-                        .col-12.col-sm-6.text-center.mb-2
-                          .form-check.form-check-inline
-                            input.form-check-input(type='radio', name='searchType', checked)
-                            label.form-check-label(for='selectHairdresser') Hairdresser
-                        .col-12.col-sm-6.text-center.mb-2
-                          .form-check.form-check-inline
-                            input.form-check-input(type='radio', name='searchType')
-                            label.form-check-label(for='selectCustomer') Customer
-                    .row.align-items-center.mx-2
-                      .col-12.col-lg-8.mb-1
-                        input.search-input.mt-3(type='text', placeholder='City, Region or Postcode')
-                      .col-12.col-lg-4.mb-1
-                        button.search-button.btn.btn-primary.mt-3(type='submit') Search
-      section.second
-        .row.g-0
-          .col.g-0
-            h1.text-center.text-secondary.mb-5 Find a mobile hairdresser close to your location
-            .row.g-0.align-items-center
-              .col-12.col-lg-3.text-center.py-2
-                img(src='@/assets/icons/hair-dryer-icon.svg', alt='hairDryerIcon')
-              .col-12.col-lg-6
-                HairdresserCard(:hairdresser='hairdresser')
-              .col-12.col-lg-3.text-center.py-2
-                img(src='@/assets/icons/scissors-icon.svg', alt='scissorsIcon')
-      section.third
-        .row.g-0
-          .col.g-0
-            h1.text-center.text-secondary.mb-5 Look at customer requests for mobile hairdressers
-            .row.g-0.align-items-center
-              .col-12.col-lg-3.text-center.py-2
-                img(src='@/assets/icons/female-icon.svg', alt='femaleIcon')
-              .col-12.col-lg-6.text-center
-                .card
-                  .card-body
-                    h5.card-title Card title
-                    p.card-text Some quick example text to build on the card title and make up the bulk of the card's content.
-                    a.btn.btn-primary(href='#') goo
-              .col-12.col-lg-3.text-center.py-2
-                img(src='@/assets/icons/male-icon.svg', alt='maleIcon')
-  footer
-    Footer
+.main
+  section.first
+    .row.g-0
+      .col.g-0
+        .position-relative
+          video.bg-video.d-block(playsinline='playsinline', autoplay='autoplay', muted='muted', loop='loop')
+            source(src='/img/videos/barbershop.mp4', type='video/mp4')
+        .search-card-container
+          .card.m-3.border-primary
+            h5.card-header.text-center I am looking for
+            .card-body
+              .form.text-center(action='submit')
+                .radiobox-container
+                  .row.align-items-center.mx-2
+                    .col-12.col-sm-6.text-center.mb-2
+                      .form-check.form-check-inline
+                        input.form-check-input(type='radio', name='searchType', checked)
+                        label.form-check-label(for='selectHairdresser') Hairdresser
+                    .col-12.col-sm-6.text-center.mb-2
+                      .form-check.form-check-inline
+                        input.form-check-input(type='radio', name='searchType')
+                        label.form-check-label(for='selectCustomer') Customer
+                .row.align-items-center.mx-2
+                  .col-12.col-lg-8.mb-1
+                    input.search-input.mt-3(type='text', placeholder='City, Region or Postcode')
+                  .col-12.col-lg-4.mb-1
+                    button.search-button.btn.btn-primary.mt-3(type='submit') Search
+  section.second
+    .row.g-0
+      .col.g-0
+        h1.text-center.text-secondary.mb-5 Find a mobile hairdresser close to your location
+        .row.g-0.align-items-center
+          .col-12.col-lg-3.text-center.py-2
+            img(src='@/assets/icons/hair-dryer-icon.svg', alt='hairDryerIcon')
+          .col-12.col-lg-6
+            HairdresserCard(:hairdresser='hairdresser')
+          .col-12.col-lg-3.text-center.py-2
+            img(src='@/assets/icons/scissors-icon.svg', alt='scissorsIcon')
+  section.third
+    .row.g-0
+      .col.g-0
+        h1.text-center.text-secondary.mb-5 Look at customer requests for mobile hairdressers
+        .row.g-0.align-items-center
+          .col-12.col-lg-3.text-center.py-2
+            img(src='@/assets/icons/female-icon.svg', alt='femaleIcon')
+          .col-12.col-lg-6.text-center
+            .card
+              .card-body
+                h5.card-title Card title
+                p.card-text Some quick example text to build on the card title and make up the bulk of the card's content.
+                a.btn.btn-primary(href='#') goo
+          .col-12.col-lg-3.text-center.py-2
+            img(src='@/assets/icons/male-icon.svg', alt='maleIcon')
 </template>
 
 <style lang="scss">
