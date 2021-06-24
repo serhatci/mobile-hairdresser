bd0bb592a986a02f22ec988b1110b160b4beccca
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-06-24 14:58:40 +0200

    Add click method to logo img

--- frontend/src/components/header.vue
+++ frontend/src/components/header.vue
@@ -2,6 +2,11 @@
 <script>
 export default {
   name: 'Header',
+  methods: {
+    goToIndex () {
+      return this.$router.push('/');
+    }
+  }
 };
 </script>
 
@@ -11,7 +16,7 @@ export default {
 <template lang="pug">
 .row.g-0
   .col-8
-    img.logo(src='../assets/logo/logo.svg', alt='logo')
+    img.logo(@click='goToIndex', src='../assets/logo/logo.svg', alt='logo')
   .col.col-4.d-flex.justify-content-end.align-items-center
     router-link.navbar-brand(to='/login') Log in
     router-link.navbar-brand.me-0(to='/signup') Sign up
