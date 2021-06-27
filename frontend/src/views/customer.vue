d1eff5795d405f8fc385f9a718d7d780666b12e1
serhat <36408498+serhatci@users.noreply.github.com>
2021-06-28 01:08:10 +0200

    Add new view pages for customer & hairdresser

---
+++ frontend/src/views/customer.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  name: 'Customer',
+}
+</script>
+
+<template lang='pug'>
+h5.m-5.p-5.text-center Welcome Customer
+</template>
---
+++ frontend/src/views/hairdresser.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  name: 'Hairdresser',
+}
+</script>
+
+<template lang='pug'>
+h5.m-5.p-5.text-center Welcome Hairdresser
+</template>
