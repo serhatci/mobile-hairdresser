eb429b2a1f083065b41e19b6a889680c1c3d1015
serhat <36408498+serhatci@users.noreply.github.com>
2021-06-27 13:50:28 +0200

    Fix issues related with @media querries

--- frontend/src/components/header.vue
+++ frontend/src/components/header.vue
@@ -14,11 +14,11 @@ export default {
 </style>
 
 <template lang="pug">
-.header.row.g-0.px-sm-3.py-sm-2.py-2
+.header.row.g-0.py-1.px-sm-1.py-sm-1
   .col-8.d-flex.justify-content-start.align-items-center
     img.logo(@click='goToIndex', src='../assets/logo/logo.svg', alt='logo')
   .col-4.d-flex.justify-content-end.align-items-center
-    .navbar.navbar-expand-md.p-0.navbar-dark.justify-content-end
+    .navbar.navbar-expand-sm.p-0.navbar-dark.justify-content-end
       button.navbar-toggler.p-0(
         type='button',
         data-bs-toggle='collapse',
@@ -30,7 +30,7 @@ export default {
         img(src='../assets/icons/list.svg', alt='navigation-list')
       #navbarSupportedContent.collapse.navbar-collapse.rounded-start
         ul.navbar-nav.px-4.py-2
-          li.nav-item.p-2
+          li.nav-item.p-2.home-item
             router-link.navbar-brand.link-light(to='/') Home
           li.nav-item.p-2
             router-link.navbar-brand.link-light(to='/login') Log in
@@ -38,9 +38,7 @@ export default {
             router-link.navbar-brand.me-0.link-light(to='/signup') Sign up
 </template>
 
-<style lang="scss" scoped>
-@import '../assets/theme.scss';
-
+<style lang="scss">
 .header {
   position: relative;
   background-color: var(--my-blue);
@@ -52,7 +50,7 @@ export default {
   background-color: var(--my-blue);
 }
 
-@media (min-width: 0px) and (max-width: 575px) {
+@media (min-width: 0px) and (max-width: 576px) {
   .logo {
     height: 35px;
   }
@@ -61,6 +59,11 @@ export default {
 @media (min-width: 576px) {
   .logo {
     height: 40px;
+  }
+
+  #navbarSupportedContent {
+    position: relative;
+    top: 0px;
   }
 }
 
