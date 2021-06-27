a630270a963dbde71268866abcda145f1717af61
serhat <36408498+serhatci@users.noreply.github.com>
2021-06-28 01:09:25 +0200

    Change account/customer endpoint to account/user

--- backend/src/routes/account.js
+++ backend/src/routes/account.js
@@ -3,28 +3,34 @@ const express = require('express')
 const router = express.Router()
 
 const Customer = require('../models/customer')
+const Hairdresser = require('../models/hairdresser')
 
 router.get('/session', (req, res) => {
   res.send(req.sessions)
 })
 
-router.post('/customer', async (req, res) => {
-  const { firstName, lastName, email, password } = req.body
+router.post('/user', async (req, res, next) => {
+  const { type, email, password, passwordConfirmation } = req.body
+  if (type !== 'Customer' && type !== 'Hairdresser') return res.status(400).send({ message: 'User type is wrong' })
+  if (password !== passwordConfirmation) return res.status(400).send({ message: 'Password confirmation is failed!' })
+  if (email === '') return res.status(400).send({ message: 'Email can not be empty!' })
+  if (password === '') return res.status(400).send({ message: 'Password can not be empty!' })
 
   try {
-    const createdCustomer = new Customer({ firstName, lastName, email })
-    await createdCustomer.setPassword(password)
-    await createdCustomer.save()
+    const createdUser =
+      type === 'Customer'
+        ? await Customer.register({ email }, password)
+        : await Hairdresser.register({ email }, password)
 
-    res.send(createdCustomer)
+    return res.send(createdUser)
   } catch (err) {
-    if (err.name === 'MongoError' && err.code === 11000) {
-      res.status(409).send({ msg: 'This user already exists!' })
-    } else if (err.name === 'ValidationError') {
-      res.status(400).send({ msg: err.errors })
-    } else {
-      res.status(500).send({ msg: 'Database query error!' })
+    if (err.name === 'UserExistsError') {
+      return res.status(409).send({ message: 'This user already exists!' })
     }
+    if (err.name === 'ValidationError') {
+      return res.status(400).send({ message: err.errors })
+    }
+    return next(err)
   }
 })
 
