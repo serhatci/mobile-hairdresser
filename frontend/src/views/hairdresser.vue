9a5b1073b0752b112a119d1276f6b75ca0bbd84b
serhat <36408498+serhatci@users.noreply.github.com>
2021-06-30 01:06:53 +0200

    Add paragraph to show user data

--- frontend/src/views/hairdresser.vue
+++ frontend/src/views/hairdresser.vue
@@ -1,9 +1,16 @@
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'Hairdresser',
+  computed: {
+    ...mapState(['user'])
+  }
 }
 </script>
 
 <template lang='pug'>
-h5.m-5.p-5.text-center Welcome Hairdresser
+div
+  h5.m-auto.p-5.text-center Welcome Hairdresser
+  p.text-center(v-if='user') USER EMAIL: {{ user.email }}
 </template>
