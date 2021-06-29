936d3468d3c05055d01f1dfaff554ef0aedb7f03
serhat <36408498+serhatci@users.noreply.github.com>
2021-06-29 09:31:49 +0200

    Add signUp action to store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,9 +4,19 @@ import axios from 'axios'
 
 Vue.use(Vuex)
 
+const mutations = {
+  SET_USER: 'set user',
+}
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    user: null,
+  },
+  mutations: {
+    [mutations.SET_USER](state, user) {
+      state.user = user
+    },
+  },
   actions: {
     async fetchIndexUsers(store) {
       const users = await axios.get('/api')
@@ -16,7 +26,23 @@ export default new Vuex.Store({
       return [customer, hairdresser]
     },
     async signUp(store, user) {
-      return axios.post('/api/account/user', user)
+      return axios.post('/api/account', user)
+    },
+    async fetchSession({ commit }) {
+      const user = await axios.get('/api/account/session')
+      commit(mutations.SET_USER, user.data || null)
+    },
+    async login({ commit }, credentials) {
+      try {
+        const user = await axios.post('/api/account/session', credentials)
+        commit(mutations.SET_USER, user.data)
+      } catch (e) {
+        throw e
+      }
+    },
+    async logout({ commit }) {
+      await axios.delete('/api/account/session')
+      commit(mutations.SET_USER, null)
     },
   },
   modules: {},
