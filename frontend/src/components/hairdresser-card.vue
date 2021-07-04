3694acaa569bc54de1061ca906e075c787769232
serhat <36408498+serhatci@users.noreply.github.com>
2021-07-04 13:52:07 +0200

    Add v-if bindings for error handling of data

--- frontend/src/components/hairdresser-card.vue
+++ frontend/src/components/hairdresser-card.vue
@@ -15,17 +15,19 @@ export default {
 </script>
 
 <template lang="pug">
-.hairdresser-card.m-auto
+.hairdresser-card.m-auto(v-if='hairdresser')
   .card
     .row.g-0
       .col-4.flex-column.flex-wrap.align-items-center.justify-content-between.p-2
         img.card-img-center.img-thumbnail.rounded-circle(src='/img/photos/hairdresser.jpg', alt='hairdresser')
-        h5.card-title.mt-1.text-center {{ hairdresser.fullName }}
+        h5.card-title.mt-1.text-center(v-if='hairdresser.fullName') {{ hairdresser.fullName }}
+        h5.text-center(v-else) No Name
       .col-8
         .card-body
           .row.g-0
             .col-6
-              p.text-start.m-0.fs-4 {{ hairdresser.city }}, {{ hairdresser.state }}
+              p.text-start.m-0.fs-4(v-if='hairdresser.city') {{ hairdresser.city }}, {{ hairdresser.state }}
+              p(v-else) Address not provided
             .col-6.text-end
               SocialMediaIcons(:hairdresser='hairdresser')
           p.text-start.mt-4 {{ hairdresser.about }}
