a8faff7d9f172b2b15c89932a071d8412885d082
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-07-04 13:59:16 +0200

    Add error handling to post router customer.js

--- backend/src/routes/customers.js
+++ backend/src/routes/customers.js
@@ -13,7 +13,18 @@ router.post('/:customerId/request', async (req, res, next) => {
   try {
     const customer = await Customer.findById(customerId)
 
-    await customer.postRequest(req.body)
+    const updatedCustomer = await customer.postRequest(req.body)
+    res.status(200).send(updatedCustomer)
+  } catch (err) {
+    if (err.name === 'MongoError' && err.code === 11000) {
+      res.status(409).send({ message: 'This request already exists!' })
+    } else if (err.name === 'ValidationError') {
+      res.status(400).send({ message: err.errors })
+    } else {
+      next(err)
+    }
+  }
+})
 
     const updatedCustomer = await customer.populate('customerRequests').execPopulate()
     res.status(200).send(updatedCustomer)
