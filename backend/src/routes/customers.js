9490afb6e304ed29cc59b94c8b919d9f0f9a3da9
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-07-04 13:59:50 +0200

    Fix delete router in customer.js

--- backend/src/routes/customers.js
+++ backend/src/routes/customers.js
@@ -26,7 +26,14 @@ router.post('/:customerId/request', async (req, res, next) => {
   }
 })
 
-    const updatedCustomer = await customer.populate('customerRequests').execPopulate()
+router.delete('/:customerId/request/:requestId', async (req, res, next) => {
+  const { customerId, requestId } = req.params
+  if (!customerId) return res.status(400).send({ message: 'User ID can not be empty!' })
+  if (!requestId) return res.status(400).send({ message: 'Request ID can not be empty!' })
+
+  try {
+    const customer = await Customer.findById(customerId)
+    const updatedCustomer = await customer.deleteRequest(requestId)
     res.status(200).send(updatedCustomer)
   } catch (err) {
     if (err.name === 'MongoError' && err.code === 11000) {
