bb04e19083737627e5a6cdd087e2468541b7fad1
serhat <36408498+serhatci@users.noreply.github.com>
2021-07-07 11:09:24 +0200

    Add search-bar.vue

---
+++ frontend/src/components/search-bar.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: 'SearchBar'
+}
+</script>
+
+<template lang='pug'>
+.search-bar.py-2
+  .col-12.col-sm-10.col-lg-8.m-auto
+    form
+      .row.g-0.align-items-center
+        .col-auto.mx-2
+          i.bi.bi-search.h4.text-body.d-none.d-sm-inline
+        .col.mx-2
+          input.form-control.form-control-md.form-control-borderless(
+            type='search',
+            placeholder='City, Region or Postcode'
+          )
+        .col-auto.mx-2
+          button.btn.btn-md.btn-success(type='submit') Search
+</template>
+
+<style>
+.search-bar {
+  background-color: var(--my-aliceblue);
+}
+</style>
