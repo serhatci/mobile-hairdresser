c547bbf4b7494c1c417291205dcaa5f424eb93f1
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-07-08 10:59:47 +0200

    Refactor search-bar component design

--- frontend/src/components/search-bar.vue
+++ frontend/src/components/search-bar.vue
@@ -5,17 +5,16 @@ export default {
 </script>
 
 <template lang='pug'>
-.search-bar.py-2
-  .col-12.col-sm-10.col-lg-8.m-auto
+.search-bar.py-2.rounded-pill
+  .col-11.col-sm-8.col-lg-8.m-auto
     form
       .row.g-0.align-items-center
-        .col-auto.mx-2
-          i.bi.bi-search.h4.text-body.d-none.d-sm-inline
-        .col.mx-2
-          input.form-control.form-control-md.form-control-borderless(
+        .col
+          input.form-control.form-control-sm.form-control-borderless(
             type='search',
             placeholder='City, Region or Postcode'
           )
         .col-auto.mx-2
-          button.btn.btn-md.btn-success(type='submit') Search
+          button.btn(type='submit')
+            i.bi.bi-search.text-body
 </template>
