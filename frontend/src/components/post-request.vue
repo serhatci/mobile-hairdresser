80dd82caefe1d7d5089586f11214fef88f921ca5
serhat <36408498+serhatci@users.noreply.github.com>
2021-07-10 01:48:10 +0200

    Initilize post-request.vue

---
+++ frontend/src/components/post-request.vue
@@ -0,0 +1,96 @@
+<script>
+import { mapState, mapActions } from 'vuex'
+import AddressInputBar from '../components/address-input-bar.vue'
+
+export default ({
+  name: 'PostRequest',
+  components: {
+    AddressInputBar
+  },
+  computed: {
+    ...mapState(['user']),
+  },
+  data () {
+    return {
+      isClicked: false,
+
+      requestType: 'Hairdresser Request',
+      title: '',
+      message: '',
+
+      backendError: null,
+    }
+  },
+  methods: {
+    ...mapActions(['postRequest', 'notifyRequest']),
+
+    async submitRequest (e) {
+      e.preventDefault()
+      try {
+        await this.postRequest({
+          sender: this.user._id,
+          requestType: this.requestType,
+          title: this.title,
+          message: this.message,
+        })
+
+        this.notifyRequest(this.user.city)
+      } catch (e) {
+        this.backendError = e.response.data.message
+      }
+    },
+  },
+})
+</script>
+
+
+<template lang="pug">
+.posts
+  .card.border-secondary.my-3.mx-2.p-3.rounded.shadow-sm
+    h6.border-bottom.pb-2.mb-0 Post a request
+      i.bi.bi-chevron-compact-up.ms-2(@click='isClicked = !isClicked', v-if='isClicked')
+      i.bi.bi-chevron-compact-down.ms-2(@click='isClicked = !isClicked', v-else)
+    transition(name='fade')
+      form.py-2(@submit='submitRequest', v-show='isClicked', autocomplete='off')
+        span.d-block.text-center.text-danger.mb-2(v-if='backendError') {{ backendError }}
+        .row.g-0
+          .col-12.col-sm-6.mb-1
+            .form-check.form-check-inline.text-muted
+              input#hairdresserRequest.form-check-input(
+                type='radio',
+                v-model='requestType',
+                value='Hairdresser Request',
+                checked33
+              )
+              label.form-check-label(for='hairdresserRequest') Hairdresser Request
+            .form-check.form-check-inline.text-muted
+              input#customerRequest.form-check-input(type='radio', v-model='requestType', value='Style Advice')
+              label.form-check-label(for='customerRequest') Style Advice
+          .col-12.col-sm-6
+            AddressInputBar
+        .mb-3
+          label.form-label(for='message')
+            span.d-none Request Message
+          textarea#message.form-control.form-control-sm(
+            v-model='message',
+            placeholder='Request Message',
+            aria-describedby='message'
+          )
+        .text-center
+          button.btn.btn-primary.w-100(type='submit') Post
+</template>
+
+<style scoped>
+i {
+  cursor: pointer;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.6s ease;
+}
+.fade-enter, .fade-leave-to
+/* .component-fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
