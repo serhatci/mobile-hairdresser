ea7cde3f1de644a022820a6ad84ccfee5e509f95
serhat <36408498+serhatci@users.noreply.github.com>
2021-07-10 01:41:09 +0200

    Initialize location.js

---
+++ backend/src/models/location.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+const LocationSchema = new mongoose.Schema({
+  country_code: {
+    type: String,
+    required: true,
+  },
+  zipcode: {
+    type: Number,
+    required: true,
+  },
+  place: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  state_code: {
+    type: String,
+    required: true,
+  },
+  province: {
+    type: String,
+    required: true,
+  },
+  province_code: {
+    type: String,
+    required: true,
+  },
+  community: {
+    type: String,
+    required: true,
+  },
+  community_code: {
+    type: Number,
+    required: true,
+  },
+  latitude: {
+    type: Number,
+    required: true,
+  },
+  longitude: {
+    type: Number,
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('Location', LocationSchema)
