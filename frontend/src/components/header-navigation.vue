774c8c62bdf967e61e8d0198cda083bbee8412a4
serhat <36408498+serhatci@users.noreply.github.com>
2021-07-12 00:00:00 +0200

    Change header nav button colors

--- frontend/src/components/header-navigation.vue
+++ frontend/src/components/header-navigation.vue
@@ -39,11 +39,11 @@ nav.navbar.navbar-expand-md.navbar-light.px-3(aria-label='Top sticky navigation 
           a.nav-link(href='/contact') Contact
       ul.navbar-nav.mb-2.mb-md-0
         li.nav-item(v-show='!user')
-          a.btn.btn-outline-primary.m-1(href='/login', role='button') Log in
+          a.btn.btn-outline-success.m-1(href='/login', role='button') Log in
         li.nav-item(v-show='!user')
-          a.btn.btn-outline-primary.m-1(href='/signup', role='button') Sign up
+          a.btn.btn-outline-success.m-1(href='/signup', role='button') Sign up
         li.nav-item(v-show='user')
-          a.btn.btn-outline-primary.m-1(href='/user', role='button') User Page
+          a.btn.btn-outline-success.m-1(href='/user', role='button') User Page
         li.nav-item(v-show='user')
-          button.btn.btn-outline-primary.m-1(@click='doLogout') Log out
+          button.btn.btn-outline-success.m-1(@click='doLogout') Log out
 </template>
