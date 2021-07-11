29e36be17decc6ad4cb199fdb7c49b21c000514c
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-07-11 12:58:02 +0200

    Change address to senderAddress

--- frontend/src/components/display-requests.vue
+++ frontend/src/components/display-requests.vue
@@ -43,7 +43,7 @@ export default {
           strong(v-if='request.requestType === "Hairdresser Request"') Mobile Hairdresser&nbsp
           strong(v-else) Style Advice&nbsp
           | at location&nbsp
-          strong {{ request.address.city }}, {{ request.address.postcode }}
+          strong {{ request.senderAddress.city }}, {{ request.senderAddress.postcode }}
         p#message.mt-3.mb-4 {{ request.message }}
         .small.d-flex.justify-content-between.pb-3.border-bottom
           nav
