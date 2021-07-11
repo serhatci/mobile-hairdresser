04b07b42add7fd800e17cb170101da70df17a451
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-07-12 00:08:16 +0200

    Remove request templates to request-card.vue

--- frontend/src/components/display-requests.vue
+++ frontend/src/components/display-requests.vue
@@ -24,39 +24,11 @@ export default {
   h6.border-bottom.pb-2.mb-0 {{ title }}
   .display-requests(v-if='requests.length > 0')
     transition-group(name='list', tag='p')
-      .requests(v-for='request in sortedRequests', :key='request._id')
-        .d-flex.flex-start.align-items-center.mt-2
-          img.rounded-circle.shadow-1-strong.me-3(
-            src='https://mdbootstrap.com/img/Photos/Avatars/img%20(9).jpg',
-            alt='avatar',
-            width='60',
-            height='60'
-          )
-          div
-            h6.fw-bold.text-primary.mb-1 {{ request.senderFullName }}
-            p.text-muted.small.mb-0
-              | Shared - {{ request.updatedAt }}
-        p.mt-3.mb-1.pb-2
-          | Looking for&nbsp
-          strong(v-if='request.requestType === "Hairdresser Request"') Mobile Hairdresser&nbsp
-          strong(v-else) Style Advice&nbsp
-          | at location&nbsp
-          strong {{ request.senderAddress.city }}, {{ request.senderAddress.postcode }}
-        p#message.mt-3.mb-4 {{ request.message }}
-        .small.d-flex.justify-content-between.pb-3.border-bottom
-          nav
-            .btn.btn-sm.me-3.text-primary.text-decoration-underline(href='#!') Comments
-            .btn.btn-sm.me-3.text-primary.text-decoration-underline(@click='clickReply') Reply
-          nav(v-show='request.senderId == user._id')
-            .btn.btn-sm.me-3.text-danger.text-decoration-underline(@click='deleteRequest(request)') DELETE
-        PostReply(:requestId='request._id')
+      .request(v-for='request in sortedRequests', :key='request._id')
+        RequestCard(:request='request')
 </template>
 
 <style scoped>
-#message {
-  white-space: pre-line;
-}
-
 .list-enter-active,
 .list-leave-active {
   transition: all 1s;
