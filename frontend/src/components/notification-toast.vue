0fefe7b645137fb3d2d1035bc33737b3b3c85c86
serhat <36408498+serhatci@users.noreply.github.com>
2021-07-25 20:43:44 +0200

    Update toast classto be stacked

--- frontend/src/components/notification-toast.vue
+++ frontend/src/components/notification-toast.vue
@@ -4,21 +4,27 @@ import { mapState } from 'vuex'
 export default {
   name: 'NotificationToast',
   computed: {
-    ...mapState(['user']),
+    ...mapState(['notifications']),
   },
 }
 </script>
 
 <template lang="pug">
-.position-fixed.bottom-0.end-0.p-3(style='z-index: 11')
-  #liveToast.toast.show.bg-warning(role='alert', aria-live='assertive', aria-atomic='true')
+.toast-container.position-fixed.bottom-0.end-0.p-3(style='z-index: 11', v-if='notifications.length')
+  .toast.show.bg-warning(
+    role='alert',
+    aria-live='assertive',
+    aria-atomic='true',
+    v-for='notification in notifications'
+  )
     .toast-header
       strong.me-auto
         i.bi.bi-bell
         span &nbspNotification
       button.btn-close(type='button', data-bs-dismiss='toast', aria-label='Close')
-    .toast-body(v-show='')
-      | A new reply for your request.
+    .toast-body
+      span(v-if='notification.type == "Reply"') You've a new reply.
+      span(v-else='notification.type=="Request"') New request in your city.
 </template>
 
 <style scoped>
