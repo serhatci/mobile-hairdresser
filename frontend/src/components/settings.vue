5b838747193c1b54e3d1617d9e7219a357c6354f
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-07-27 21:55:45 +0200

    Add .settings-bg class && background image

--- frontend/src/components/settings.vue
+++ frontend/src/components/settings.vue
@@ -30,39 +30,39 @@ export default {
 </script>
 
 <template lang="pug">
-form.mt-4
-  span.d-block.text-center.text-danger.mb-2(v-if='backendError') {{ backendError }}
-  .row.mb-3.text-sm-end
-    label.col-sm-2.col-form-label(for='inputFirstName') First Name
-    .col-sm-10
-      input#inputFirstName.form-control.form-control-sm(
-        type='text',
-        :placeholder='user.firstName',
-        v-model='firstName'
-      )
-  .row.mb-3.text-sm-end
-    label.col-sm-2.col-form-label(for='inputLastName') Last Name
-    .col-sm-10
-      input#inputLastName.form-control.form-control-sm(type='text', :placeholder='user.lastName', v-model='lastName')
-  .row.mb-3.text-sm-end
-    span.col-sm-2 Address
-    #addressInput.col-sm-10
-      AddressInputBar(@clicked='getLocation', key='Settings Address', :userAddress='user.address.city')
-  .row.mb-3
-    .col-sm-2
-    .col-sm-10.text-start
-      button.btn.btn-primary.me-4(type='submit') Update
-      button.btn.btn-warning(type='submit') Cancel
+.settings-bg
+  form.mt-4.m-auto(@submit='sendUserUpdate', autocomplete='off')
+    .d-block.text-start.text-danger.mb-2(v-if='backendError') {{ backendError }}
+    .row.mb-2
+      .col-12
+        label.col-form-label(for='inputFirstName', :class='{ "text-danger": !user.firstName }') * First Name:
+          strong.text-success.ps-2 {{ user.firstName }}
+      .col-12.col-sm-8.col-lg-6
+        input#inputFirstName.form-control.form-control-sm(type='text', v-model='firstName')
+    .row.mb-2
+      .col-12
+        label.col-form-label(for='inputLastName', :class='{ "text-danger": !user.lastName }') * Last Name:
+          strong.text-success.ps-2 {{ user.lastName }}
+      .col-12.col-sm-8.col-lg-6
+        input#inputLastName.form-control.form-control-sm(type='text', v-model='lastName')
+    .row.mb-4
+      span.col-12.py-1(:class='{ "text-danger": !user.address.city }') * Address:
+        strong.text-success.ps-2(v-if='user.address.city') {{ user.address.city }}, {{ user.address.postcode }}
+      .col-12.col-sm-8.col-lg-6
+        AddressInputBar(@clicked='getLocation', key='Settings Address')
+    .row.mb-3
+      .col-12.col-sm-8.col-lg-6.text-end.mb-5
+        button.btn.btn-primary(type='submit') Update
 </template>
 
 <style scoped>
-input {
-  max-width: 22.5rem;
-}
-#addressInput {
-  max-width: 24rem;
-}
 label {
   white-space: nowrap;
+}
+form {
+  max-width: 50rem;
+}
+.settings-bg {
+  background-image: url('../assets/illustrations/undraw-barber.svg');
 }
 </style>
