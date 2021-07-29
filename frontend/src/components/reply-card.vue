17b54228eeecb24004d1898c2065cc99dae6a3a8
serhat <36408498+serhatci@users.noreply.github.com>
2021-07-30 01:13:11 +0200

    Fix reply-card

--- frontend/src/components/reply-card.vue
+++ frontend/src/components/reply-card.vue
@@ -21,23 +21,20 @@ export default ({
 </script>
 
 <template lang="pug">
-.clearfix
-  .card.border.mt-1.me-2.px-3.bg-light(:class='{ "float-start": sameUser, "float-end": !sameUser }')
-    .row.mt-1
-      .col-10
-        .d-flex.flex-start.align-items-center
-          i#personIcon.bi.bi-person-circle.text-muted.me-2
-          div
-            h6.fw-bold.mb-1(:class='{ "text-info": sameUser, "text-body": !sameUser }') {{ reply.senderFullName }}
-              #sendPM.d-inline-block.text-primary.text-decoration-underline.ms-3.p-1(v-show='user.type=="Customer"') Send PM
-            p.text-muted.small.mb-0
-              | Shared - {{ reply.createdAt | formatDate }}&nbsp
-              | {{ reply.senderAddress ? reply.senderAddress.city : "Unknown" }}
+.card.border.mt-1.me-2.px-3.bg-light
+  .row.mt-1
+    .col-10
+      .d-flex.flex-start.align-items-center
+        i#personIcon.bi.bi-person-circle.text-muted.me-2
+        div
+          h6.fw-bold.mb-1(:class='{ "text-info": sameUser, "text-body": !sameUser }') {{ reply.senderFullName }}
+          p.text-muted.small.mb-0
+            | Shared - {{ reply.createdAt | formatDate }}&nbsp
+            | {{ reply.senderAddress ? reply.senderAddress.city : "Unknown" }}
 
-      .col-2.text-end
-        nav.d-inline-block(v-if='reply.senderId == user._id')
-          .btn.btn-sm.me-3.text-danger.text-decoration-underline(@click='deleteReplyCard(reply._id)') Delete
-    p#message.mt-3.mb-4 {{ reply.message }}
+    .col-2.text-end
+      a.btn.btn-sm.me-3.text-danger(v-if='reply.senderId == user._id', @click='deleteReplyCard(reply._id)') Delete
+  p#message.my-3.ms-0.ms-sm-5 {{ reply.message }}
 </template>
 
 <style lang='scss' scoped>
