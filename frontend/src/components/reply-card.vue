8cf555fa63e11b8aae16b81d7bcd4115e67de25b
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-08-02 04:17:02 +0200

    Import Portfolio component to reply-card component

--- frontend/src/components/reply-card.vue
+++ frontend/src/components/reply-card.vue
@@ -1,18 +1,19 @@
 <script>
 import { mapState } from 'vuex'
+import Portfolio from "@/components/portfolio.vue";
 
 export default ({
   name: 'ReplyCard',
+  components: {
+    Portfolio
+  },
   props: {
     reply: {},
-    requestId: String,
-    sameUser: Boolean,
   },
   computed: {
     ...mapState(['user']),
   },
   methods: {
-
     deleteReplyCard (replyId) {
       this.$emit('reply-deleted', replyId)
     }
@@ -21,23 +22,33 @@ export default ({
 </script>
 
 <template lang="pug">
-.card.border.mt-1.me-2.px-3.bg-light
-  .row.mt-1
-    .col-10
-      .d-flex.flex-start.align-items-center
-        i#personIcon.bi.bi-person-circle.text-muted.me-2
-        div
-          h6.fw-bold.mb-1(:class='{ "text-info": sameUser, "text-body": !sameUser }') {{ reply.senderFullName }}
-          p.text-muted.small.mb-0
-            | Shared - {{ reply.createdAt | formatDate }}&nbsp
-            | {{ reply.senderAddress ? reply.senderAddress.city : "Unknown" }}
-
-    .col-2.text-end
-      a.btn.btn-sm.me-3.text-danger(v-if='reply.senderId == user._id', @click='deleteReplyCard(reply._id)') Delete
-  p#message.my-3.ms-0.ms-sm-5 {{ reply.message }}
+div
+  Portfolio(v-if='reply.senderType == "Hairdresser"', :hairdresser='reply.senderId')
+  .card.border.mt-1.me-2.px-3.bg-light
+    .row
+      .col-12.col-sm-10
+        .d-flex.flex-start.align-items-center
+          i#personIcon.bi.bi-person-circle.text-muted.me-2
+          div
+            a(
+              v-if='reply.senderType == "Hairdresser"',
+              data-bs-toggle='modal',
+              data-bs-target='#portfolioModal',
+              aria-label='Portfolio',
+              href='#!'
+            )
+              h6.fw-bold.mb-1.text-success {{ reply.senderFullName }}
+                i.bi.bi-scissors.m-2
+            h6.fw-bold.mb-1.text-dark(v-else) {{ reply.senderFullName }}
+            p.small.mb-0
+              | Shared - {{ reply.createdAt | formatDate }}&nbsp
+      .col-12.col-sm-2.text-end
+        a.btn.btn-sm.text-danger(v-if='reply.senderId == user._id', @click='deleteReplyCard(reply._id)') Delete
+        address.text-muted.small {{ reply.senderCity ? `${reply.senderCity}, ${reply.senderPostcode}` : "Unknown" }}
+    p#message.my-3.ms-0.ms-sm-5 {{ reply.message }}
 </template>
 
-<style lang='scss' scoped>
+<style scoped>
 #message {
   white-space: pre-line;
 }
