863b1ce1b63b5f4a82c342177eb954e0a22f7975
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-08-22 22:40:38 +0200

    Add AddressInputBar in to template

--- frontend/src/components/search-bar.vue
+++ frontend/src/components/search-bar.vue
@@ -22,13 +22,14 @@ export default {
 <template lang='pug'>
 .search-bar.py-2.rounded-pill
   .col-11.col-sm-8.col-lg-8.m-auto
-    form
+    form(:action='getSearchUrl', autocomplete='off')
       .row.g-0.align-items-center
         .col
-          label.d-none(for='search') Search
-          input#search.form-control.form-control-sm.form-control-borderless(
-            type='search',
-            placeholder='City, Region or Postcode'
+          AddressInputBar(
+            @clicked='geoLocation = $event',
+            @update='geoLocation = { city: $event }',
+            inputId='searchAddress',
+            ref='addressBar'
           )
         .col-auto.mx-2
           button.btn(type='submit', aria-label='submit')
